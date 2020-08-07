1a93b5eac11b71f86e94fca303d75c6018099a7f
Fiona Ochs <fiona.oaks@gmail.com>
2020-08-07 10:26:30 -0700

    pre helper functions

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const csv = require('csvtojson');
+const createCsvWriter = require('csv-writer').createArrayCsvWriter;
 
 const silverPlans = {};
 const zip_to_rate_areas = {};
+let zip_to_second_silver = [];
 
 csv()
   // find plans that are silver
@@ -20,7 +22,7 @@ csv()
             silverPlans[row.rate_area] = 
               [row.rate].concat(silverPlans[row.rate_area][0]);
           }
-          if(row.rate < silverPlans[row.rate_area][1]){
+          if(row.rate !== silverPlans[row.rate_area][0] && row.rate < silverPlans[row.rate_area][1]){
             silverPlans[row.rate_area] = 
               [silverPlans[row.rate_area][0]].concat(row.rate);
           }
@@ -71,12 +73,38 @@ csv()
               if(key_rate_area.size === 1){
                 const rate = Array.from(key_rate_area)[0];
                 const smallest_silver_rates = silverPlans[rate];
-                console.log('zipcode', row.zipcode, 'rate', smallest_silver_rates[1]);
+                if(smallest_silver_rates.length === 0){
+                  console.log(smallest_silver_rates);
+                }
+
+                // console.log('silver', smallest_silver_rates);
+                zip_to_second_silver = zip_to_second_silver.concat([[row.zipcode, smallest_silver_rates[1]]]);
               }
+
+              
               else if(key_rate_area.size > 1){
-                const blankRate =
+                // console.log(key_rate_area, row.zipcode);
+                zip_to_second_silver = zip_to_second_silver.concat([[row.zipcode]]);
+                // console.log('2', zip_to_second_silver);
+                // 40813,.
               }
+
             });
+          })
+          // add errror reject 
+          .then(() => {
+            const csvWriter = createCsvWriter({
+              header: ['zipcode', 'rate'],
+              path: './output.csv'
+            });
+            csvWriter.writeRecords(zip_to_second_silver)
+              .then(() => {
+                // console.log('csv complete');
+              });
+            // console.log('csv zip', zip_to_second_silver);
+          })
+          .catch((err) => {
+            // console.log(err);
           });
       });
   });
