1953eaaf72ec36dffd2bab87e887cc387ec72f0e
Fiona Ochs <fiona.oaks@gmail.com>
2020-08-09 11:13:49 -0700

    rename

--- zip.py
+++ zip.py
@@ -1,6 +1,6 @@
 from zipfile import ZipFile
 
-zipObj = ZipFile('solution.zip', 'w')
+zipObj = ZipFile('AdHocCodeChallenge.zip', 'w')
 zipObj.write('solution.py')
 zipObj.write('COMMENTS.md')
 zipObj.write('plans.csv')
